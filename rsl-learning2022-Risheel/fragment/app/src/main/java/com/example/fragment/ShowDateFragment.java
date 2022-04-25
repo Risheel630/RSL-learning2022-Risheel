00b42c77614097fb6d53c1c184db41beb22db85f
risheel30 <bhalanirishil89@gmail.com>
2022-04-25 10:10:11 +0530

    fragment asssignment done

---
+++ rsl-learning2022-Risheel/fragment/app/src/main/java/com/example/fragment/ShowDateFragment.java
@@ -0,0 +1,22 @@
+package com.example.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class ShowDateFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v= inflater.inflate(R.layout.common_fragment,container,false);
+        TextView time=(TextView) v.findViewById(R.id.textView);
+        time.setText("23.04.2022");
+        return v;
+    }
+}
